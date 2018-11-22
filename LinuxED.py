3b7a6102cb1ad0cddb9a606002ef04777cde73da
Cr3atable <31049082+Cr3atable@users.noreply.github.com>
2018-11-21 23:17:51 -0500

    Update LinuxED.py

--- LinuxED.py
+++ LinuxED.py
@@ -1,4 +1,4 @@
-#the shittiest thing ever (BUT IT STILL WORKS SO IT'S OKAY)
+#the shittiest thing ever (BUT IT STILL WORKS SO IT'S OKAY) TESTING UPDATES PLEASE FUCKING WORK
 import os
 import pwd
 from shutil import copyfile
