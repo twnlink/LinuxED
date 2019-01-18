e33e9eb3d5883ba42225c25b4234acc197b9fb30
Cr3atable <31049082+Cr3atable@users.noreply.github.com>
2019-01-18 14:14:34 -0500

    Fix for 0.0.8

--- LinuxED.py
+++ LinuxED.py
@@ -17,40 +17,41 @@ injdir = 'process.env.injDir = \'%s/EnhancedDiscord\';'%dirpath
 
 # this is not my code but it's what I put at the end of index.js
 patch = """%s
-const { BrowserWindow, session } = require('electron');
+const electron = require('electron');
 const path = require('path');
 
-session.defaultSession.webRequest.onHeadersReceived(function(details, callback) {
-
-   if (!details.responseHeaders["content-security-policy-report-only"] && !details.responseHeaders["content-security-policy"]) return callback({cancel: false});
-   delete details.responseHeaders["content-security-policy-report-only"];
-
-   delete details.responseHeaders["content-security-policy"];
-
-   callback({cancel: false, responseHeaders: details.responseHeaders})
-;
+electron.session.defaultSession.webRequest.onHeadersReceived(function(details, callback) {
+    if (!details.responseHeaders["content-security-policy-report-only"] &&
+    !details.responseHeaders["content-security-policy"]) return callback({cancel: false});
+    delete details.responseHeaders["content-security-policy-report-only"];
+    delete details.responseHeaders["content-security-policy"];
+    callback({cancel: false, responseHeaders: details.responseHeaders});
 });
 
-class PatchedBrowserWindow extends BrowserWindow {
-   constructor(originalOptions) {
-       const options = Object.create(originalOptions);
-       options.webPreferences = Object.create(options.webPreferences);
-    
-    const originalPreloadScript = options.webPreferences.preload;
+class BrowserWindow extends electron.BrowserWindow {
+    constructor(originalOptions) {
+        if (!originalOptions || !originalOptions.webPreferences|| !originalOptions.title) return super(originalOptions);
+        const options = Object.create(originalOptions);
+        options.webPreferences = Object.create(options.webPreferences);
+        
+        const originalPreloadScript = options.webPreferences.preload;
 
-       // Make sure Node integration is enabled
-       options.webPreferences.nodeIntegration = true;
-       options.webPreferences.preload = path.join(process.env.injDir, 'dom_shit.js');
-       options.webPreferences.transparency = true;
+        // Make sure Node integration is enabled
+        options.webPreferences.nodeIntegration = true;
+        options.webPreferences.preload = path.join(process.env.injDir, 'dom_shit.js');
+        options.webPreferences.transparency = true;
 
-       super(options);
-       this.__preload = originalPreloadScript;
-   }
+        super(options);
+        this.__preload = originalPreloadScript;
+    }
 }
 
 const electron_path = require.resolve('electron');
+Object.assign(BrowserWindow, electron.BrowserWindow); // Assigns the new chrome-specific ones
+const newElectron = Object.assign({}, electron, {BrowserWindow});
+require.cache[electron_path].exports = newElectron;
 const browser_window_path = require.resolve(path.resolve(electron_path, '..', '..', 'browser-window.js'));
-require.cache[browser_window_path].exports = PatchedBrowserWindow;
+require.cache[browser_window_path].exports = BrowserWindow;
 module.exports = require('./core.asar');"""%injdir
 
 # Tcll: should we really be using static versions??
